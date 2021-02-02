17610aba4bccd67907f75aa26d983ae1e3f9bd8c
W. Guilherme <ravok123@gmail.com>
2021-02-02 11:58:32 -0300

    [app] added ping/pong response

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const { token } = require("./token.json")
 
-client.on("ready", () => {
+client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
+//    await client.channels.get('806175198763417651').send('Hello here!');
 });
+
+// Create an event listener for messages
+client.on('message', message => {
+    // If the message is "ping"
+    if (message.content === 'ping') {
+      // Send "pong" to the same channel
+      message.channel.send('pong');
+    }
+  });
+
 
 client.on("message", msg => {
 
@@ -12,5 +23,9 @@ client.on("message", msg => {
         msg.reply("Hello world!");
     }
 });
+
+
+
+
 
 client.login(token);
