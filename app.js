c67604a4e4b9180448bae76201c3ae7c3fcc24b2
W. Guilherme <ravok123@gmail.com>
2021-02-03 17:59:20 -0300

    [update] added express, simple rest api, send msg route with msg and name of the channel

--- app.js
+++ app.js
@@ -2,10 +2,46 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const { token } = require("./token.json")
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json())
+
+
 client.on("ready", async () => {
     console.log(`Logged in as ${client.user.tag}!`);
 //    await client.channels.get('806175198763417651').send('Hello here!');
 });
+
+app.listen(3000, ()=>{
+    console.log('App listening on port 3000')
+})
+
+
+client.on('ready',  () => {
+    // Routes
+    app.post('/msg/:channelName', async (req, res) =>{    
+        const {message} = req.body
+        const {channelName} = req.params    
+
+        try {
+            await sendMessage(message, channelName)        
+            
+        } catch (error) {
+            res.status(400).json(error.message)
+        }
+        res.status(200).json({message: "Message sent successfully"})    
+    })    
+})
+
+function sendMessage(message, channelName){    
+    const channel =  client.channels.cache.find(channel => channel.name === channelName)
+    channel.send(message)
+    
+    console.log('Message sent')
+}
 
 // Create an event listener for messages
 client.on('message', message => {
@@ -32,11 +68,11 @@ client.on("message", msg => {
 
 
 
-client.on('ready', () => {
-    const channel = client.channels.cache.find(channel => channel.name === 'bot-lab')
-    channel.send('Lorem ipsum')
-    console.log(channel.name)
-})
+// client.on('ready', () => {
+//     const channel = client.channels.cache.find(channel => channel.name === 'bot-lab')
+//     channel.send('Lorem ipsum')
+//     console.log(channel.name)
+// })
 
 
 client.on('ready', () => {
