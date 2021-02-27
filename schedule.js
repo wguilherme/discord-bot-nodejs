63958ed391c7f31a298a1199afa4afd871c265fd
W. Guilherme <ravok123@gmail.com>
2021-02-27 01:21:04 -0300

    [update] added simple schedule study file

---
+++ schedule.js
@@ -0,0 +1,5 @@
+const schedule = require('node-schedule');
+
+const job = schedule.scheduleJob('*/3 * * * * *', function(){
+   console.log('The answer to life, the universe, and everything!');
+ });
